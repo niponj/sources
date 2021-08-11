84bf1720bf89aed2d65ea2a8161e9eb7eb7a99c1
niponj <39453000+niponj@users.noreply.github.com>
2021-08-11 13:46:09 +0700

    Add files via upload

---
+++ terraform-modules/main.tf
@@ -0,0 +1,76 @@
+# Terraform configuration
+
+provider "aws" {
+  region = "us-west-2"
+  
+  ## v Everything between the comments is localstack specific v
+  access_key                  = "anaccesskey"
+  secret_key                  = "asecretkey"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway = "http://localhost:4566"
+    ec2        = "http://localhost:4566"
+    s3         = "http://localhost:4566"
+  }
+  ## ^ Everything between the comments is localstack specific ^
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "my-ec2-cluster"
+  instance_count = 2
+
+  ami                    = "ami-0c5204531f799e0c6"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "robin-test-dec-17-2019"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
